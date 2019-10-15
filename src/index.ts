cd98689f5cf7534ddb501ec9ee329586a2b42f3b
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-14 22:05:36 -0700

    :construction: Add basic process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export default "A";
+import cosmiconfig from "cosmiconfig";
+import { success, error } from "signale";
+import { StaartSiteConfig } from "./interfaces";
+
+const explorer = cosmiconfig("staart");
+
+const staartSite = async () => {
+  const searchResult = await explorer.search();
+  const config: StaartSiteConfig = searchResult ? searchResult.config : {};
+};
+
+const startTime = new Date().getTime();
+staartSite()
+  .then(() =>
+    success(
+      `Start Site built in ${(
+        (new Date().getTime() - startTime) /
+        1000
+      ).toFixed(2)}s`
+    )
+  )
+  .catch(err => error(err));
---
+++ src/interfaces.ts
@@ -0,0 +1,3 @@
+export interface StaartSiteConfig {
+  title?: string;
+}

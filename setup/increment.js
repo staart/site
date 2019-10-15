1988be9ccafdf7b00da2e739782609725f24c1b4
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-14 21:16:18 -0700

    :tada: Add package.json

---
+++ setup/increment.js
@@ -0,0 +1,23 @@
+const fs = require("fs-extra");
+const path = require("path");
+const { error } = require("signale");
+
+const incrementVersion = async () => {
+  const pkg = JSON.parse(
+    (await fs.readFile(path.join(__dirname, "..", "package.json"))).toString()
+  );
+  const newVersion = pkg.version
+    .split(".")
+    .map((a, i) => (i === 2 ? parseInt(a) + 1 : a))
+    .join(".");
+  pkg.version = newVersion;
+  await fs.writeFile(
+    path.join(__dirname, "..", "package.json"),
+    JSON.stringify(pkg, null, 2)
+  );
+};
+
+incrementVersion()
+  .then(() => {})
+  .catch(err => error(err))
+  .then(() => process.exit(0));

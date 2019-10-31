43395032524fa38f9fd60d45e8014220cd3577b7
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-31 13:26:31 -0700

    :sparkles: Support for light/dark toggle

--- src/staart.js
+++ src/staart.js
@@ -1,1 +1,40 @@
-console.log("Hello, world!");
+if (
+  (window.matchMedia("(prefers-color-scheme: dark)") ||
+    localStorage.getItem("theme") === "dark") &&
+  localStorage.getItem("theme") !== "light"
+)
+  document.documentElement.classList.add("dark");
+
+function updateStorage() {
+  localStorage.setItem(
+    "theme",
+    document.documentElement.classList.contains("dark") ? "dark" : "light"
+  );
+}
+
+var toggleButton = document.querySelector(".theme-toggle");
+if (toggleButton) {
+  toggleButton.addEventListener("click", function() {
+    document.documentElement.classList.toggle("dark");
+    updateStorage();
+    updateText();
+  });
+}
+
+function updateText() {
+  if (toggleButton) {
+    toggleButton.setAttribute(
+      "aria-label",
+      document.documentElement.classList.contains("dark")
+        ? "Change theme to light"
+        : "Change theme to dark"
+    );
+    toggleButton.setAttribute(
+      "title",
+      document.documentElement.classList.contains("dark")
+        ? "Change theme to light"
+        : "Change theme to dark"
+    );
+  }
+}
+updateText();

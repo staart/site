28ca4a864e27ae90df3da63eb11d94e32b27691d
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-24 16:51:07 +0530

    :heavy_minus_sign: Remove del-cli

--- scripts/staart.ts
+++ scripts/staart.ts
@@ -1,13 +1,45 @@
 import { join } from "path";
-import {} from "fs-extra";
-import {} from "typescript";
+import { ensureDir, copyFile, copy, readdir } from "fs-extra";
+import {
+  CompilerOptions,
+  createProgram,
+  getPreEmitDiagnostics,
+  flattenDiagnosticMessageText,
+} from "typescript";
+
+const compile = (fileNames: string[], options: CompilerOptions) => {
+  const program = createProgram(fileNames, options);
+  const emitResult = program.emit();
+  const allDiagnostics = getPreEmitDiagnostics(program).concat(
+    emitResult.diagnostics
+  );
+  allDiagnostics.forEach((diagnostic) => {
+    if (diagnostic.file) {
+      let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start!
+      );
+      let message = flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      );
+    } else {
+      console.log(flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
+    }
+  });
+};
 
 const staart = async () => {
-  await mkdir(join(".", ".staart"), { recursive: true });
-  await copyFile(
-    join(".", ".eleventy.ts"),
-    join(".", ".staart", ".eleventy.ts")
-  );
+  await ensureDir(join(".", ".staart"));
+  const files = await readdir(join("."));
+  for await (const file of files) {
+    await copyFile(join(".", file), join(".", ".staart", file));
+  }
+  for await (const dir of ["eleventy", "src"]) {
+    await copy(join(".", dir), join(".", ".staart", dir));
+  }
+  compile([join(".", ".staart", ".eleventy.ts")], {
+    esModuleInterop: true,
+  });
 };
 
 staart();

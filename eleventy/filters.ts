04f8e8be21a3b78119f9c6b12befc7a2411a2952
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-08 15:08:33 +0530

    :recycle: Use custom safeUrl filter for links

--- eleventy/filters.ts
+++ eleventy/filters.ts
@@ -1,29 +1,35 @@
 import { DateTime } from "luxon";
 import CleanCSS from "clean-css";
 const HRNumbers = require("human-readable-numbers");
+import pkg from "../package.json";
+const settings: any = pkg["@staart/site"] || {};
 
 export default {
-  dateToFormat: function(date: Date, format: string) {
+  dateToFormat: function (date: Date, format: string) {
     return DateTime.fromJSDate(date, { zone: "Asia/Kolkata" }).toFormat(
       String(format)
     );
   },
 
-  dateToISO: function(date: Date) {
+  dateToISO: function (date: Date) {
     return DateTime.fromJSDate(date, { zone: "Asia/Kolkata" }).toISO({
       includeOffset: false,
       suppressMilliseconds: true,
     });
   },
 
-  cssmin: function(css: string) {
+  safeUrl: function (url: string) {
+    return (settings.baseUrl || "") + url;
+  },
+
+  cssmin: function (css: string) {
     return new CleanCSS({ level: 2 }).minify(css).styles;
   },
 
-  numberToLocale: function(num: string) {
+  numberToLocale: function (num: string) {
     return parseInt(num, 10).toLocaleString();
   },
-  numberToFloat: function(num: string) {
+  numberToFloat: function (num: string) {
     return parseFloat(num).toLocaleString();
   },
   numberToReadable: (num: string | number) => HRNumbers.toHumanString(num),
